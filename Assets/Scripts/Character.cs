0c35e17596fb4a2c432c559a1715397c3e70bbdd
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-08-06 14:22:50 +0500

    now a player is able to shoot by single shots and by fire burst

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,7 +5,11 @@ public class Character : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
     [SerializeField] private int hp;
+
     [SerializeField] private Projectile bulletPrefab;
+    [SerializeField] private Transform pointOfBulletSpawn;
+    [SerializeField] private int amountOfBulletsInBurst;
+    [SerializeField] private float timeBetweenShotsInBurst;
 
     public void Move(float inputX)
     {
@@ -17,9 +21,17 @@ public class Character : MonoBehaviour
         hp -= damageValue;
     }
 
-    public void Shot()
+    public void ShootOnce()
     {
-        var spawnedBullet = Instantiate(bulletPrefab);
+        var spawnedBullet = Instantiate(bulletPrefab, pointOfBulletSpawn.transform.position, Quaternion.identity);
         spawnedBullet.SetVelocity();
+    }
+
+    public void FireBurst()
+    {
+        for(int i = 0; i < amountOfBulletsInBurst; i++)
+        {
+            Invoke(nameof(ShootOnce), i * timeBetweenShotsInBurst);
+        }
     }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,14 +3,45 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Character character;
+    [SerializeField] private float requredHoldingTimeToFireBurst = 0.1f;
+    private float timeOfHoldingShotButton;
+    private bool ableToMakeFireBurstInput = true;
 
     private void Update()
     {
-        character.Move(Input.GetAxisRaw("Horizontal"));
+        Movement();
+        Shooting();
+    }
 
-        if (Input.GetMouseButtonDown(0))
+    private void Movement()
+    {
+        character.Move(Input.GetAxisRaw("Horizontal"));
+    }
+
+    private void Shooting()
+    {
+        if (Input.GetMouseButton(0))
         {
-            character.Shot();
+            timeOfHoldingShotButton += Time.deltaTime;
+        }
+
+        if (ableToMakeFireBurstInput && timeOfHoldingShotButton > requredHoldingTimeToFireBurst)
+        {
+            character.FireBurst();
+            ableToMakeFireBurstInput = false;
+            return;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(timeOfHoldingShotButton <= requredHoldingTimeToFireBurst)
+            {
+                character.ShootOnce();
+            }
+
+            ableToMakeFireBurstInput = true;
+            timeOfHoldingShotButton = 0;
+            return;
         }
     }
 }
