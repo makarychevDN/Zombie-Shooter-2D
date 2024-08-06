a3092b2c1adb0899be25792f57ca6cfaafe26429
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-08-06 15:41:15 +0500

    characters are able to rotate their sprites due movement input now

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -6,6 +6,7 @@ public class Character : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
     [SerializeField] private int hp;
+    [SerializeField] private Transform sprite;
 
     public UnityEvent<int> OnHealthDecreased;
     public UnityEvent OnHPEnded;
@@ -16,6 +17,11 @@ public class Character : MonoBehaviour
     public void Move(float inputX)
     {
         rb.velocity = new Vector2 (inputX * speed, 0);
+
+        if (inputX == 0)
+            return;
+
+        sprite.rotation = Quaternion.Euler(0, inputX < 0 ? 180 : 0, 0);
     }
 
     public void ApplyDamage(int damageValue)
